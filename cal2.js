9f003d58a0ff4a34ddda3508911dcd8d7df94776
anasshah2000 <shahanas4069@gmail.com>
2024-12-03 13:49:05 +0530

    Add files via upload
    project index

---
+++ cal2.js
@@ -0,0 +1,90 @@
+var x, y, result
+ var myInputBox= document.getElementById("num1")
+function input1(){
+   
+    myInputBox.value+= "1"
+}
+function input2(){
+   
+    myInputBox.value+= "2"
+}
+function input3(){
+   
+    myInputBox.value+= "3"
+}
+function input4(){
+   
+    myInputBox.value+= "4"
+}
+function input5(){
+   
+    myInputBox.value+= "5"
+}
+function input6(){
+   
+    myInputBox.value+= "6"
+}
+function input7(){
+   
+    myInputBox.value+= "7"
+}
+function input8(){
+   
+    myInputBox.value+= "8"
+}
+function input9(){
+   
+    myInputBox.value+= "9"
+}
+function add(){
+   
+    myInputBox.value+= "+"
+}
+function sub(){
+   
+    myInputBox.value+= "-"
+}
+function mul(){
+   
+    myInputBox.value+="*"
+}
+function div(){
+   
+    myInputBox.value+="/"
+}
+function cal(){
+   
+    myInputBox.value = eval(
+        myInputBox.value)
+}
+function sin(){
+   
+    myInputBox.value= Math.sin(
+        myInputBox.value)
+}
+function cos(){
+   
+    myInputBox.value= Math.cos(
+        myInputBox.value)
+}
+function tan(){
+   
+    myInputBox.value= Math.tan(
+        myInputBox.value)
+}
+function cot(){
+   
+    myInputBox.value= Math.cot(
+        myInputBox.value)
+}
+function coses(){
+   
+    myInputBox.value= Math.coses(
+        myInputBox.value)
+}
+function ac(){
+   
+    myInputBox.value= ""
+}
+
+
