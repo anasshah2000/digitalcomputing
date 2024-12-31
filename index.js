58466b11ec654cd9111d7ecdeded40b569d2208e
anasshah2000 <shahanas4069@gmail.com>
2024-12-31 14:09:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+
+
+const circle = document.getElementById('circle');
+
+
+setTimeout(function() {
+    
+    circle.style.backgroundColor = 'yellow ';
+}, 2000); 
+
+
+
+
+function light() {
+    document.getElementById('mode').style.background="linear-gradient( to right,rgb(82, 141, 209),rgb(179, 37, 54))";
+    
+    
+}
+function dark() {
+    document.getElementById('mode').style.background="linear-gradient( to right,rgb(110, 236, 215),rgb(226, 149, 222))";
+    
+    
+}
+
+var z
+    
+    function change() {
+        z = document.getElementById('img').style.background=" url('tree.jpeg') "; 
+        z= document.getElementById('img').style.backgroundImage=" "
+    }
+        
+        
